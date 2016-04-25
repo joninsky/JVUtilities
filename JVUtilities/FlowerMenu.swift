8645c9a139f9cd56d05d5f170c7e10c5d08ec62a
Jon Vogel <joninsky@gmail.com>
2016-04-24 23:48:27 -0700

    With Good Flower Menu. Some Stuff with Flower Menu Controller.

--- JVUtilities/FlowerMenu.swift
+++ JVUtilities/FlowerMenu.swift
@@ -9,11 +9,9 @@
 import UIKit
 
 public protocol FlowerMenuDelegate {
-    func flowerMenuDidSelectPedalWithID(theMenu: FlowerMenu, identifier: String)
-    func flowerMenuShouldExpand(theMenu: FlowerMenu) -> Bool
-    func flowerMenuDidExpand(theMenu: FlowerMenu)
-    func flowerMenuShouldRetract(theMenu: FlowerMenu) -> Bool
-    func flowerMenuDidRetract(theMenu: FlowerMenu)
+    func flowerMenuDidSelectPedalWithID(theMenu: FlowerMenu, identifier: String, pedal: UIView)
+    func flowerMenuDidExpand()
+    func flowerMenuDidRetract()
 }
 
 public enum Position {
@@ -24,17 +22,17 @@ public enum Position {
     case Center
 }
 
-public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
+public class FlowerMenu: UIImageView {
     
     //MARK: Public Variables
     public var centerView: UIView!
     public var pedals: [UIView] = [UIView]()
     public var pedalSize: CGFloat!
-    public var pedalDistance: CGFloat!
-    public var pedalSpace: CGFloat!
+    public var pedalDistance: CGFloat = 100
+    public var pedalSpace: CGFloat = 50
     public var startAngle: CGFloat!
-    public var growthDuration: NSTimeInterval!
-    public var stagger: NSTimeInterval!
+    public var growthDuration: NSTimeInterval = 0.4
+    public var stagger: NSTimeInterval = 0.06
     public var menuIsExpanded: Bool = false
     public var delegate: FlowerMenuDelegate?
     public var showPedalLabels = false
@@ -46,23 +44,20 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
     //MARK: Private Variables
     var pedalIDs: [String: UIView] = [String: UIView]()
     var positionView: UIView!
-    var theSuperView: UIView!
+    //var theSuperView: UIView!
     
     //MARK: Init functions
     public init(withPosition: Position, andSuperView view: UIView, andImage: UIImage?){
         super.init(image: andImage)
         self.translatesAutoresizingMaskIntoConstraints = false
         self.userInteractionEnabled = true
-        self.theSuperView = view
-        self.theSuperView?.addSubview(self)
+        //self.theSuperView = view
+        //self.superview?.addSubview(self)
+        view.addSubview(self)
         self.constrainToPosition(withPosition, animate: false)
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(FlowerMenu.didTapCenterView(_:)))
         self.addGestureRecognizer(tapGesture)
         self.currentPosition = withPosition
-        self.pedalDistance = 100
-        self.pedalSpace = 50
-        self.stagger = 0.06
-        self.growthDuration = 0.4
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -72,16 +67,12 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
     
     //MARK: Public functions
     public func addPedal(theImage: UIImage, identifier: String){
-        
         let newPedal = self.createAndConstrainPedal(theImage, name: identifier)
-        
         self.addSubview(newPedal)
-        self.constrainPedal(newPedal)
-        newPedal.center = CGPointMake(self.bounds.origin.x + self.bounds.size.width / 2, self.bounds.origin.y + self.bounds.size.height / 2)
+        self.constrainPedalToSelf(newPedal)
         self.pedals.append(newPedal)
         self.pedalIDs[identifier] = newPedal
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(FlowerMenu.didTapPopOutView(_:)))
-        tapGesture.delegate = self
         newPedal.addGestureRecognizer(tapGesture)
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(FlowerMenu.pannedViews(_:)))
         newPedal.addGestureRecognizer(panGesture)
@@ -106,15 +97,12 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
     
     public func grow(){
         for (index, view) in self.pedals.enumerate() {
-            
             let indexAsDouble = Double(index)
-            
             UIView.animateWithDuration(self.growthDuration, delay: self.stagger * indexAsDouble, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.4, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
                 view.alpha = 1
-                print(self.getTransformForPopupViewAtIndex(CGFloat(index)))
                 view.transform = self.getTransformForPopupViewAtIndex(CGFloat(index))
                 }, completion: { (didComplete) in
-                    self.delegate?.flowerMenuDidExpand(self)
+                    self.delegate?.flowerMenuDidExpand()
             })
         }
         self.menuIsExpanded = true
@@ -122,30 +110,24 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
     
     public func shrivel(){
         for (index, view) in self.pedals.enumerate() {
-            
             let indexAsDouble = Double(index)
-            
             UIView.animateWithDuration(self.growthDuration, delay: self.stagger * indexAsDouble, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.4, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
                 view.alpha = 0
                 view.transform = CGAffineTransformIdentity
                 }, completion: { (didComplete) in
-                    self.delegate?.flowerMenuDidExpand(self)
+                    self.delegate?.flowerMenuDidRetract()
             })
         }
         self.menuIsExpanded = false
     }
     
-    public func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
-        print("Gesture")
-        return true
-    }
-    
     internal func didTapPopOutView(sender: UITapGestureRecognizer) {
         for key in self.pedalIDs.keys {
-            let view = self.pedalIDs[key]
+            guard let view = self.pedalIDs[key] else{
+                break
+            }
             if view == sender.view {
-                print("Tapped Pop Out named: " + key)
-                self.delegate?.flowerMenuDidSelectPedalWithID(self, identifier: key)
+                self.delegate?.flowerMenuDidSelectPedalWithID(self, identifier: key, pedal: view)
             }
         }
     }
@@ -159,7 +141,56 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
     }
     
     internal func pannedViews(sender: UIPanGestureRecognizer) {
-        print("Detecting Pan")
+
+        guard let theView = sender.view else {
+            return
+        }
+        
+        var indexOfView: Int?
+        
+        for (index, view) in self.pedals.enumerate() {
+            if view == theView{
+                indexOfView = index
+                break
+            }
+        }
+        
+        let point = sender.locationInView(self)
+        
+        let centerX = self.bounds.origin.x + self.bounds.size.width / 2
+        
+        let centerY = self.bounds.origin.y + self.bounds.size.height / 2
+        
+        if sender.state == UIGestureRecognizerState.Changed {
+            
+            let deltaX = point.x - centerX
+            
+            let deltaY = point.y - centerY
+            
+            let atan = Double(atan2(deltaX, -deltaY))
+            
+            let angle = atan * Double(180) / M_PI
+            
+            self.pedalDistance = sqrt(pow(point.x - centerX, 2) + pow(point.y - centerY, 2))
+            
+            self.startAngle = CGFloat(angle) - self.pedalSpace * CGFloat(indexOfView!)
+            
+            theView.center = point
+            
+            theView.transform = CGAffineTransformIdentity
+            
+            for (index, aView) in self.pedals.enumerate() {
+                if aView != theView {
+                    aView.transform = self.getTransformForPopupViewAtIndex(CGFloat(index))
+                }
+            }
+            
+        }else if sender.state == UIGestureRecognizerState.Ended {
+            theView.center = CGPointMake(centerX, centerY)
+            for (index, aView) in self.pedals.enumerate() {
+                aView.transform = self.getTransformForPopupViewAtIndex(CGFloat(index))
+            }
+        }
         
     }
     
@@ -211,8 +242,8 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
         var arrayOfConstraints = [NSLayoutConstraint]()
         switch thePosition {
         case .Center:
-            let verticalConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.theSuperView, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0)
-            let horizontalConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.theSuperView, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)
+            let verticalConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.superview, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0)
+            let horizontalConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.superview, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)
             arrayOfConstraints.append(verticalConstraint)
             arrayOfConstraints.append(horizontalConstraint)
             self.startAngle = 0
@@ -222,14 +253,14 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
             let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[me]-28-|", options: [], metrics: nil, views: ["me":self])
             arrayOfConstraints.appendContentsOf(horizontalConstraints)
             arrayOfConstraints.appendContentsOf(verticalConstraints)
-            self.startAngle = 0
+            self.startAngle = 10
             print("Constrain To Lower Left")
         case .LowerRight:
             let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[me]-28-|", options: [], metrics: nil, views: ["me": self])
             let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[me]-28-|", options: [], metrics: nil, views: ["me":self])
             arrayOfConstraints.appendContentsOf(horizontalConstraints)
             arrayOfConstraints.appendContentsOf(verticalConstraints)
-            self.startAngle = -90
+            self.startAngle = -80
             print("Constrain To Lower Right")
         case .UpperLeft:
             let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-28-[me]", options: [], metrics: nil, views: ["me": self])
@@ -237,13 +268,13 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
             arrayOfConstraints.appendContentsOf(horizontalConstraints)
             arrayOfConstraints.appendContentsOf(verticalConstraints)
             print("Constrain To Upper Left")
-            self.startAngle = 90
+            self.startAngle = 100
         case .UpperRight:
             let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[me]-28-|", options: [], metrics: nil, views: ["me": self])
             let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-28-[me]", options: [], metrics: nil, views: ["me":self])
             arrayOfConstraints.appendContentsOf(horizontalConstraints)
             arrayOfConstraints.appendContentsOf(verticalConstraints)
-            self.startAngle = 180
+            self.startAngle = 170
             print("Constrain To Upper Right")
         }
         
@@ -252,7 +283,7 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
         arrayOfConstraints.append(width)
         arrayOfConstraints.append(height)
         
-        self.theSuperView.addConstraints(arrayOfConstraints)
+        self.superview?.addConstraints(arrayOfConstraints)
         self.setNeedsLayout()
         
         if animate {
@@ -292,7 +323,7 @@ public class FlowerMenu: UIImageView, UIGestureRecognizerDelegate {
         thePedal.addConstraints(arrayOfConstraint)
     }
     
-    private func constrainPedal(thePedal: UIView) {
+    private func constrainPedalToSelf(thePedal: UIView) {
         
         var arrayOfConstraints = [NSLayoutConstraint]()
         
--- JVUtilities/FlowerMenuController.swift
+++ JVUtilities/FlowerMenuController.swift
@@ -8,25 +8,45 @@
 
 import UIKit
 
+@objc
+public protocol PedalProtocol {
+    var pedalImage: UIImage? { get set }
+    var pedalName: String? { get set }
+}
 
 
 //Make a Container View Controller
 //https://developer.apple.com/library/ios/featuredarticles/ViewControllerPGforiPhoneOS/ImplementingaContainerViewController.html
 
-public class FMViewControler: UIViewController, UIViewControllerTransitioningDelegate {
+public class FMViewControler: UIViewController, UIViewControllerTransitioningDelegate, FlowerMenuDelegate {
     
     //MARK: Properties
     
-    var nextViewButton: UIButton?
+    public private(set) var flowerMenu: FlowerMenu!
+    public var viewControllers = [UIViewController]()
+    
     
     //MARK: Lifecycle Functions
     public override func viewDidLoad() {
         super.viewDidLoad()
-        self.nextViewButton = UIButton(type: UIButtonType.Custom)
-        self.nextViewButton?.setTitle("NextVC", forState: UIControlState.Normal)
-        self.nextViewButton?.addTarget(self, action: #selector(FMViewControler.nextVCPressed(_:)), forControlEvents: UIControlEvents.TouchUpInside)
-        self.nextViewButton?.frame = CGRectMake(0, 0, 100, 100)
-        self.view.addSubview(self.nextViewButton!)
+        
+        guard let menuImage = UIImage(named: "MenuIconGood") else {
+            return
+        }
+        
+        self.flowerMenu = FlowerMenu(withPosition: .UpperRight, andSuperView: self.view, andImage: menuImage)
+        self.flowerMenu.showPedalLabels = true
+        self.flowerMenu.delegate = self
+        
+        guard let messageImage = UIImage(named: "Messages"), let notificationImage = UIImage(named: "Notifications"), let profileImage = UIImage(named: "Profile") else {
+            return
+        }
+        
+//        self.flowerMenu.addPedal(messageImage, identifier: "Messages")
+//        self.flowerMenu.addPedal(notificationImage, identifier: "Notifications")
+//        self.flowerMenu.addPedal(profileImage, identifier: "Profile")
+        
+        
         
     }
     
@@ -35,39 +55,68 @@ public class FMViewControler: UIViewController, UIViewControllerTransitioningDel
         super.viewWillAppear(animated)
     }
     
-    func nextVCPressed(sender: UIButton) {
-        //self.performSegueWithIdentifier("toNext", sender: self)
-        print("Next Pressed")
-    }
     
     
     public override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        self.performSegueWithIdentifier("toMap", sender: self)
     }
-    //MARK: Container Functions
     
+    //MARK: Custom Functions
+    
+    public func addViewController(viewController: UIViewController, withPedalImage image: UIImage, withPedalTitle title: String){
+        
+        //self.flowerMenu.addPedal(image, identifier: title)
+        
+        self.addChildViewController(viewController)
+        
+    }
+    
+    //MARK: Flower Menu Delegate Functions
+    public func flowerMenuDidSelectPedalWithID(theMenu: FlowerMenu, identifier: String, pedal: UIView){
+        
+        print("Touched pedal named: " + identifier )
+        
+        
+        
+        
+        
+    }
+    public func flowerMenuDidExpand(){
+        print("Flower Menu Expended")
+    }
+    public func flowerMenuDidRetract(){
+        print("Flower Menu Collapsed")
+    }
+    
+    
+    //MARK: Container Functions
     public override func addChildViewController(childController: UIViewController) {
         
+        print("Add Child View Called")
         
+        childController.view.frame = self.view.frame
+
+        self.view.insertSubview(childController.view, atIndex: 0)
+        
+        childController.didMoveToParentViewController(self)
     }
     
     
     public override func removeFromParentViewController() {
         
-        
+        print("Remove From Parent View")
     }
     
     
     public override func willMoveToParentViewController(parent: UIViewController?) {
-        
+        print("Will Move To Parent View")
         
         
     }
     
     public override func didMoveToParentViewController(parent: UIViewController?) {
         
-        
+        print("Did Move To Parent View")
         
         
     }
